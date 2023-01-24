29cf4c4e22233305d8b8fe6fe9cb228d0797fb65
Aribunfi <arianna@pabis.de>
2023-01-24 08:45:17 +0100

    correzione concatenazione

--- act.js
+++ act.js
@@ -1,18 +1,18 @@
 //chiedi nome
 
 const userName = prompt('Qual è il tuo nome?')
-console.log('Qual è il tuo nome?' userName);
+console.log('Qual è il tuo nome?' + userName);
 
 //chiedi cognome
 
 const userSurname = prompt('Qual è il tuo cognome?')
-console.log('Qual è il tuo cognome?' userSurname);
+console.log('Qual è il tuo cognome?' + userSurname);
 
 
 //chiedi colore preferito
 
 const favColor = prompt('Qual è il tuo colore preferito?')
-console.log('Qual è il tuo colore preferito?' favColor);
+console.log('Qual è il tuo colore preferito?' + favColor);
 
 
 //restituisci: nome+cognome+colore+23
